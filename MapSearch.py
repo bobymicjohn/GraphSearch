3259fc3546ad572c71d4bf757dea08c216fc3166
Jordan Brack <jbrack14@gmail.com>
2017-03-19 15:18:21 -0400

    Removed bad files and poll user for input matrix.

--- MapSearch.py
+++ MapSearch.py
@@ -29,6 +29,19 @@ def buildGraph(csvpath):
 
 
 def main():
+	path = 'Assign2_MapMatrix.csv'
+	isNewPath = 'x'
+	while(1):
+		print 'Loading %s would you like to use a different adjacency matrix .csv file path?' % (path)
+		isNewPath = raw_input("[y/n]: ")
+		if (isNewPath == 'y' or isNewPath == 'Y') :
+			path = raw_input("Please enter file path to valid .csv file: ")
+			break
+		elif (isNewPath == 'n' or isNewPath == 'N') :
+			break
+		else:
+			print 'Error: invalid selection\n'
+	
 	graph = buildGraph('Assign2_MapMatrix.csv')
 	print graph
 	
